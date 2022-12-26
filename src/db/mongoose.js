07e56e0aacdb7c3412f7f5f6c65a3d1977cc3211
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2022-12-26 07:12:28 +0000

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const session = require("express-session");
-const express = require('express');
-const MongoDBSession = require("connect-mongodb-session")(session);
 
 
 // mongoose setup
@@ -11,11 +8,3 @@ mongoose.connect(process.env.MONGODB_URL, {
 }).then((res) => {
     console.log("mongodb connected");
 });
-
-const Store = new MongoDBSession({
-    uri: process.env.MONGODB_URL,
-    collection: "mySession"
-});
-
-
-module.exports = Store
